a5db6c196cf6faac936b028a225234bbac73a261
Martin Hradil <himdel@seznam.cz>
2012-12-22 23:27:39 +0000

    MapItem done

--- MapItem.cpp
+++ MapItem.cpp
@@ -1,17 +1,28 @@
+#include <assert.h>
 #include "MapItem.hpp"
 
 
 MapItem::MapItem(std::string popis) {
+	this->popis = popis;
+	this->map = NULL;
 }
 
 void
 MapItem::place(Map<MapItem> *map, int x, int y) {
+	this->map = map;
+	this->x = x;
+	this->y = y;
+	this->map->put(x, y, this);
 }
 
 void
 MapItem::remove() {
+	assert(this->map);
+	assert(this->map->get(x, y) == this);
+	this->map->put(x, y, NULL);
 }
 
-virtual std::string
+std::string
 MapItem::getPopis() {
+	return this->popis;
 }
