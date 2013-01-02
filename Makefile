ca8da2e08911b1ac3225f3d403017d89dee862c9
Martin Hradil <himdel@seznam.cz>
2013-01-02 23:49:09 +0000

    Map is templated, can't have Map.cpp -> merge into hpp

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-CXX=g++ -std=c++11
+CXX=g++ -std=c++11 -Wall
 LD=true
 
-# warciv: Axethrower.o Building.o Forest.o Game.o GoldMine.o Grunt.o Map.o MapItem.o Peon.o Player.o Unit.o WatchTower.o main.o units.o buildings.o Resource.o UI.o
+# warciv: Axethrower.o Building.o Forest.o Game.o GoldMine.o Grunt.o MapItem.o Peon.o Player.o Unit.o WatchTower.o main.o units.o buildings.o Resource.o UI.o
 #	$(LD) -o $@ $^ $(LDFLAGS)
 
-warciv: Map.o main.o units.o buildings.o Game.o Player.o MapItem.o UI.o Resource.o Forest.o GoldMine.o Unit.o Building.o WatchTower.o
+warciv: main.o units.o buildings.o Game.o Player.o MapItem.o UI.o Resource.o Forest.o GoldMine.o Unit.o Building.o WatchTower.o
 	$(LD) -o $@ $^ $(LDFLAGS)
 
 clean:
--- Map.hpp
+++ Map.hpp
@@ -2,25 +2,63 @@
 #define __MAP_HPP__
 
 #include <vector>
+using namespace std;
 
 template <typename T>
 class Map {
 private:
-	std::vector< std::vector< T* > > data;
+	vector< vector< T* > > data;
 	int width;
 	int height;
 
 public:
-	Map(int w, int h);
-	T *get(int x, int y);
-	void put(int x, int y, T* elem);
+	Map(int w, int h) {
+		this->data.resize(w);
+		for (int i = 0; i < w; i++)
+			this->data[i].resize(h, NULL);
+
+		this->width = w;
+		this->height = h;
+	}
+
+	T *get(int x, int y) {
+		if ((x >= this->width) || (y >= this->height)) {
+			cerr << "Map::get out of bounds " << x << ", " << y << endl;
+			return NULL;
+		}
+
+		return this->data[x][y];
+	}
+
+	void put(int x, int y, T* elem) {
+		if ((x >= this->width) || (y >= this->height)) {
+			cerr << "Map::put out of bounds " << x << ", " << y << endl;
+			return;
+		}
+
+		this->data[x][y] = elem;
+	}
 
 	int getWidth() { return this->width; }
 	int getHeight() { return this->height; }
 
-	void show();
+	void show() {
+		for (int y = 0; y < this->height; y++) {
+			for (int x = 0; x < this->width; x++)
+				cout << this->data[x][y] << "\t\t";
 
-	~Map();
+			cout << endl;
+		}
+	}
+
+	~Map() {
+		for (int x = 0; x < this->width; x++) {
+			for (int y = 0; y < this->height; y++) {
+				delete this->data[x][y];
+				this->data[x][y] = NULL;
+			}
+		}
+	}
 };
 
 #endif	// __MAP_HPP__
