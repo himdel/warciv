2128fe7ba0b08692d7add9c3fd639ccf86e1e5d2
Martin Hradil <himdel@seznam.cz>
2013-01-08 22:57:19 +0000

    fixes v ami

--- ruby/AttackMapItem.rb
+++ ruby/AttackMapItem.rb
@@ -12,65 +12,58 @@ class AttackMapItem < MapItem
 		@attack_range = 0
 	end
 
-	virtual void damage(hitpoints)
+	def damage(hitpoints)
+		@hitpoints -= min(@hitpoints, hitpoints)
 
-	virtual bool attack()
-	virtual bool attack(x, y)
-	virtual bool attack(i)
-end
-
-
-def damage(hitpoints)
-	@hitpoints -= min(@hitpoints, hitpoints)
-
-	if (@hitpoints == 0)
-		self.remove()
-end
-
-
-def attack()
-	bool bb = false
-	@owner.getEnemies().each do |e|
-		e.getUnits().each do |u|
-			if (self.distance(u) <= @attack_range)
-				bb |= AttackMapItem::attack(u)
-		end
-
-		e.getBuildings().each do |b|
-			if (self.distance(b) <= @attack_range)
-				bb |= AttackMapItem::attack(b)
-		end
+		if (@hitpoints == 0)
+			self.remove()
 	end
-	return bb
-end
 
-def attack(x, y)
-	MapItem i = @map.get(x, y)
-	if (!i)
-		return false
+	def attack()
+		bool bb = false
+		@owner.getEnemies().each do |e|
+			e.getUnits().each do |u|
+				if (self.distance(u) <= @attack_range)
+					bb |= AttackMapItem::attack(u)
+			end
 
-	AttackMapItem a = dynamic_cast<AttackMapItem >(i)
-	if (!a)
-		return false
+			e.getBuildings().each do |b|
+				if (self.distance(b) <= @attack_range)
+					bb |= AttackMapItem::attack(b)
+			end
+		end
+		return bb
+	end
 
-	return self.attack(a)
-end
+	def attack(x, y)
+		MapItem i = @map.get(x, y)
+		if (!i)
+			return false
 
-# do not use i afterwards
-def attack(i)
-	if (i == self)
-		return false
+		AttackMapItem a = dynamic_cast<AttackMapItem >(i)
+		if (!a)
+			return false
 
-	if (!@attack_damage || !@attack_range)
-		return false
+		return self.attack(a)
+	end
 
-	if (self.distance(i.x, i.y) > @attack_range)
-		return false
+	# do not use i afterwards
+	def attack(i)
+		if (i == self)
+			return false
 
-	double d = 2
-	if (self.distance(i.x, i.y) - 1 > @attack_range / 2.0)
-		d = 1
+		if (!@attack_damage || !@attack_range)
+			return false
 
-	i.damage( rand() % ((int) round(@attack_damage * d)) )
-	return true
+		if (self.distance(i.x, i.y) > @attack_range)
+			return false
+
+		double d = 2
+		if (self.distance(i.x, i.y) - 1 > @attack_range / 2.0)
+			d = 1
+
+		i.damage( rand() % ((int) round(@attack_damage * d)) )
+		return true
+	end
+
 end
